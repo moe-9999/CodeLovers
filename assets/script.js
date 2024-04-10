13123a51db46069aa36614698696d5a025d9d8b5
moe <143455790+moe-9999@users.noreply.github.com>
2024-04-10 22:55:48 +0200

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+// const accordionButtons = document.querySelectorAll('.accordion-item button');
+
+// accordionButtons.forEach((button) => {
+//   button.addEventListener('click', () => {
+//     const accordionItem = button.closest('.accordion-item');
+//     const accordionBody = accordionItem.querySelector('.accordion-collapse');
+
+//     if (accordionBody.classList.contains('show')) {
+//       console.log('shown');
+//     }
+//   });
+// });
